b12faa433f4f8a9d2d0dab1283574eec39e575ab
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-06 00:07:13 +0530

    Edit the listed item

--- app/src/main/java/com/example/grocery_list/UI/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/grocery_list/UI/RecyclerViewAdapter.java
@@ -6,14 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.grocery_list.Activities.DetailsActivity;
+import com.example.grocery_list.Data.DatabaseHandler;
 import com.example.grocery_list.Model.Grocery;
 import com.example.grocery_list.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -21,6 +25,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context context;
     private List<Grocery> groceryItems;
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
+    private LayoutInflater inflater;
 
     public RecyclerViewAdapter(Context context, List<Grocery> groceryItems) {
         this.context = context;
@@ -97,16 +104,88 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             switch (view.getId()) {
                 case R.id.editButton:
-
+                    int position = getAdapterPosition();
+                    Grocery grocery = groceryItems.get(position);
+                    editItem(grocery);
                     break;
 
                 case R.id.deleteButton:
-
+                    position = getAdapterPosition();
+                    grocery = groceryItems.get(position);
+                    deleteItem(grocery.getId());
                     break;
             }
         }
-        public void deleteItem(int id){
 
+        public void editItem(Grocery grocery){
+
+            alertDialogBuilder = new AlertDialog.Builder(context);
+            inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.popup, null);
+
+            final EditText groceryItem = (EditText) view.findViewById(R.id.enterItem);
+            final EditText quantity = (EditText) view.findViewById(R.id.enterQuantity);
+            Button saveButton = (Button) view.findViewById(R.id.saveButton);
+            final TextView title = (TextView) view.findViewById(R.id.title);
+
+            title.setText("Edit Grocery");
+            alertDialogBuilder.setView(view);
+            dialog = alertDialogBuilder.create();
+            dialog.show();
+
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DatabaseHandler db = new DatabaseHandler(context);
+
+                    //update item
+                    grocery.setName(groceryItem.getText().toString());
+                    grocery.setQuantity(quantity.getText().toString());
+
+                    if(!groceryItem.getText().toString().isEmpty()
+                    && !quantity.getText().toString().isEmpty()){
+                        db.updateGrocery(grocery);
+                        notifyItemChanged(getAdapterPosition(),grocery);
+                    } else {
+                        Snackbar.make(view, "Add Grocery & Quantity", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+        public void deleteItem(final int id){
+
+            //create alert dialog
+            alertDialogBuilder = new AlertDialog.Builder(context);
+            inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.confirmation_dialog, null);
+
+            Button noButton = (Button) view.findViewById(R.id.noButton);
+            Button yesButton = (Button) view.findViewById(R.id.yesButton);
+
+            alertDialogBuilder.setView(view);
+            dialog = alertDialogBuilder.create();
+            dialog.show();
+
+            noButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    dialog.dismiss();
+                }
+            });
+
+            yesButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //delete item
+                    DatabaseHandler db = new DatabaseHandler(context);
+                    db.deleteGrocery(id);
+                    groceryItems.remove(getAdapterPosition());
+                    notifyItemRemoved(getAdapterPosition());
+
+                    dialog.dismiss();
+                }
+            });
         }
     }
 }
