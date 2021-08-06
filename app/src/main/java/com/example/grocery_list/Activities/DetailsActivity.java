a9722b35d860ba66363a444d93360455493b9734
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-06 16:04:53 +0530

    Update activity_details.xml

--- app/src/main/java/com/example/grocery_list/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/grocery_list/Activities/DetailsActivity.java
@@ -1,11 +1,19 @@
 package com.example.grocery_list.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.grocery_list.Data.DatabaseHandler;
+import com.example.grocery_list.Model.Grocery;
 import com.example.grocery_list.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -13,6 +21,13 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView quantity;
     private TextView dateAdded;
     private int groceryId;
+    private Button editButton;
+    private Button deleteButton;
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
+    private Button saveButton;
+    private LayoutInflater inflater;
+    private DatabaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +38,10 @@ public class DetailsActivity extends AppCompatActivity {
         quantity = (TextView) findViewById(R.id.quantityDet);
         dateAdded = (TextView) findViewById(R.id.dateDet);
 
+        db = new DatabaseHandler(this);
+
         Bundle bundle = getIntent().getExtras();
-        if( bundle !=null){
+        if (bundle != null) {
             itemName.setText(bundle.getString("name"));
             quantity.setText(bundle.getString("quantity"));
             dateAdded.setText(bundle.getString("date"));
